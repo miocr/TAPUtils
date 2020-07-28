a78205e28198dffc2f55414631eb875309bafb2e
miocr <omihula@post.cz>
2020-07-28 19:43:30 +0200

    1.0

--- Program.cs
+++ Program.cs
@@ -11,7 +11,15 @@ namespace TAPUtils
         {
             if (args.Length < 1 || !File.Exists(args[0]))
             {
-                Console.WriteLine("Usage...");
+                Console.WriteLine("");
+                Console.WriteLine("Extract codeblocks from TAP file, specialy blocks with Tasword or D-TEXT(SpectralWriter). CIDSOFT (C)2017, version 1.0");
+                Console.WriteLine("");
+                Console.WriteLine("Usage: dotnet run file");
+                Console.WriteLine("");
+                if (!File.Exists(args[0]))
+                {
+                    Console.WriteLine($"File not found ({args[0]}).");
+                }
                 return;
             }
 
